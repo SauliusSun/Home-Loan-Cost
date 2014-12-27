651f26f2ad193bfda3f89296bddf21df10f6402e
SauliusSun <saulius.sunauskas@gmail.com>
2014-12-27 17:53:35 +0200

    Defining technology stack: .NET Framework 4.5, C# 5, ASP.NET MVC 5

---
+++ HomeLoanCost/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HomeLoanCost")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeLoanCost")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("211216e2-b500-43d1-a2da-4c7255703f83")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
